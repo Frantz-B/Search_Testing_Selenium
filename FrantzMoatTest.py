b454f7832b3cc67375ef89b73ee7a971fece0c15
Frantz <Frantz-b@github.com>
2016-11-19 02:06:47 -0500

    There's a lot going on here.
    - One of the last things I figure out (quickly I might add) is that the mouse hover functionality using Actionchains class.
    
    - Also worked on the logic for making sure the load_more_button appear so i know that I need to click on it again to load the full list in the search results page
    
    - Worked on the logic for detecting a word string inside another string, of which is inside a list.  Because apparently when I looked for 'min' inside [ '22' , 'mins', 'ago' ], this turned up false orignially.  Will test again.
    
    - Also to a crack at using the page object design and also using unittest class
    
        Personal note - Question 3 took me forever and I asked for help.  1st time dealing with a dynamic grid & it didn't help when trying to find pic elements but they was automatically popup when mouse goes on them.  Asked for help and person I asked said that this actually probably can't be done.  I was very proud when I figured it out on my own!!!!  Let's me know that I'm not far off.

--- FrantzMoatTest.py
+++ FrantzMoatTest.py
@@ -1,43 +1,139 @@
 from selenium import webdriver
+import time
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
 
 chromeDriverLoc = "/Users/NewUser/Downloads/Automation/chromedriver"
 mURL = 'http://www.moat.com'
 
+cdriver = webdriver.Chrome(chromeDriverLoc)
+cdriver.get(mURL)
+
+cdriver.find_element_by_id("pro-landing-search-box").send_keys('better' + Keys.RETURN)
+time.sleep(2)
+loadMeBtn = cdriver.find_element_by_id("paginate-button")
+x = 0
+
+insideBox = cdriver.find_element_by_class_name('ad-cell')
+hover = ActionChains(cdriver).move_to_element(insideBox)
+hover.perform()
+time.sleep(2)
+damn = cdriver.find_element_by_xpath('//*[@id="popup-template"]/div/div[1]/div[2]/table/tbody/tr[5]/td[2]/div/a')
+damn.click()
+damn2 = cdriver.find_element_by_xpath('//*[@id="popup-template"]/div/div[1]/div[2]/table/tbody/tr[5]/td[2]/div/div/input')
+print(damn2.is_displayed())
+"""
+while loadMeBtn.is_displayed() :
+    loadMeBtn.click()
+    x = x + 1
+    print("This was clicked :", x, " times")
+    time.sleep(4)
+insideBox = cdriver.find_elements_by_class_name('ad-cell')
+#insideBox = cdriver.find_element_by_css_selector('#all > div.ads-container > div.ads').get_attribute('div')
+print(len(insideBox))
+# shit comet ff fuck rocket zata zz tops (get # createvies by span via ele by css"""
+
+
+"""from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+
+mList2 = ['https://wwww.moat.com', 'http://moat.com', 'https://moat.com/search_lucky?q=%7C%7C&doc_type=advertiser']
+def verifyWorkingLnks(lnksList):
+    cnt = 0
+    for x in lnksList:
+        req = Request(x)
+        try:
+            response = urlopen(req)
+        except HTTPError as e:
+            print('Link Validation Function FAILED with following lnk: ', x, ' Status code given: ', e.code)
+        else:
+            cnt += 1
+            if cnt == 3 :
+                print('Link Validation Function:  PASSED')
+
+eleText = '22 mins ago'
+x = eleText.split()
+print(x)
+print(int(x[0]))
+num = int(x[0])
+if (any('min' in dic for dic in x)) & (num <30) :
+    print('exitcue')
+else:
+    print('what thsfuk')
+
+mylist = ["2 min ago", "54 secs ago", '30 secs ago', '0.5 hours ago']
+
+def verifyRecentAdTime(AdverList):
+    episolon = 0
+    for x in AdverList:
+        p = x.split()
+        n = float(p[0])
+        if (any('min' in c for c in p)) & (n < 30) :
+            episolon += 1
+        elif (any('sec' in c for c in p)) & (n < 60*30) :
+            episolon += 1
+        elif (any('hour' in c for c in p)) & (n < 0.5):
+            episolon += 1
+    if episolon == 4 :
+        print("pass")
+    else: print('fail', episolon)
+
+verifyRecentAdTime(mylist)
+
+//*[@id="popup-template"]/div
+//*[@id="popup-template"]/div/div[1]/div[1]/div/img
+//*[@id="popup-template"]/div/div[1]/div[2]
+//*[@id="popup-template"]/div/div[1]/div[2]"""
+
+"""from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+chromeDriverLoc = "/Users/NewUser/Downloads/Automation/chromedriver"
+mURL = 'https://www.tumblr.com/search/naruto'
 
 cdriver = webdriver.Chrome(chromeDriverLoc)
 cdriver.get(mURL)
 
-# captured Web Elements
-mSearchBox = cdriver.find_element_by_id("pro-landing-search-box")
-mTryLink1 = cdriver.find_element_by_xpath('//*[@id="search-bar"]/div/div[1]/span/a[1]')
-mTryLink2 = cdriver.find_element_by_xpath('//*[@id="search-bar"]/div/div[1]/span/a[2]')
-mTryLink3 = cdriver.find_element_by_xpath('//*[@id="search-bar"]/div/div[1]/span/a[3]')
+#cdriver.find_element_by_id("pro-landing-search-box").send_keys('Betterment' + Keys.RETURN)
+insideBox = cdriver.find_elements_by_tag_name('article')
+print(len(insideBox))"""
 
-# List of captured Try These links
-mList = [mTryLink1.text, mTryLink2.text, mTryLink3.text]
-mList2 = [mTryLink1.text, mTryLink2.text, mTryLink3.text]
+#labels = driver.find_elements_by_tag_name("label")
+#inputs = driver.execute_script(
+#    "var labels = arguments[0], inputs = []; for (var i=0; i < labels.length; i++){" +
+#   "inputs.push(document.getElementById(labels[i].getAttribute('for'))); } return inputs;", labels)
 
-def somehit(caplist , othlist):
-    #caplist = mList
-    print(caplist, othlist)
-somehit(mList , mList2)
-print(mTryLink2 , " ", mTryLink1)
+import unittest
+import selenium
+import page
 
-def verifyRandom(tryTheseList):
-    cdriver.refresh()
+class PythonOrgSearch(unittest.TestCase):
+    """A sample test class to show how page object works"""
 
-    mTryLinkRefresh1 = cdriver.find_element_by_xpath('//*[@id="search-bar"]/div/div[1]/span/a[1]')
-    mTryLinkRefresh2 = cdriver.find_element_by_xpath('//*[@id="search-bar"]/div/div[1]/span/a[2]')
-    mTryLinkRefresh3 = cdriver.find_element_by_xpath('//*[@id="search-bar"]/div/div[1]/span/a[2]')
-    mList2 = [mTryLinkRefresh1.text, mTryLinkRefresh2.text, mTryLinkRefresh3.text]
-    list(set(tryTheseList) & set(mList2))
-    if not list:
-        print('Random Function:  PASSED')
-    else:
-        print('Random Function:  FAILED')
+    def setUp(self):
+        self.driver = selenium.webdriver.Firefox()
+        self.driver.get("http://www.python.org")
 
-mList3 = [mTryLink4.text, mTryLink5.text]
-mList4 = [mTryLink4.text, mTryLink5.text]
-somehit(mList3 , mList4)
-print(mTryLink5 , " ", mTryLink4)
+    def test_search_in_python_org(self):
+        """
+        Tests python.org search feature. Searches for the word "pycon" then verified that some results show up.
+        Note that it does not look for any particular text in search results page. This test verifies that
+        the results were not empty.
+        """
+
+        #Load the main page. In this case the home page of Python.org.
+        main_page = page.MainPage(self.driver)
+        #Checks if the word "Python" is in title
+        assert main_page.is_title_matches(), "python.org title doesn't match."
+        #Sets the text of search textbox to "pycon"
+        main_page.search_text_element = "pycon"
+        main_page.click_go_button()
+        search_results_page = page.SearchResultsPage(self.driver)
+        #Verifies that the results page is not empty
+            assert search_results_page.is_results_found(), "No results found."
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
