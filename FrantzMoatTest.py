95e4b9dcfe78acfb683cb22a33d4fad0150fa701
Frantz <Frantz-b@github.com>
2016-11-18 21:08:29 -0500

    Me messing with site I'm putting under test and making sure Chromedriver is playing nice

--- FrantzMoatTest.py
+++ FrantzMoatTest.py
@@ -1,45 +1,19 @@
-from urllib.request import Request, urlopen
-from urllib.error import URLError, HTTPError
-    
-class thisworksbetter(object):
- 	"""docstring for thisworksbetter"""
- 	def __init__(self, arg):
- 		super(thisworksbetter, self).__init__()
- 		self.arg = arg
- 		 thisworksbetter:
- 
-    req = Request('http://eatadick')
-    try:
-        response = urlopen(req)
-    except HTTPError as e:
-        print('The server couldn\'t fulfill the request.')
-        print('Error code: ', e.code)
-    except URLError as e:
-        print('We failed to reach a server.')
-        print('Reason: ', e.reason)
-    else:
-        print('everything is fine')
-        mSearchBox = cdriver.find_element_by_id("pro-landing-search-box")
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-# sublime added lines 5-9 to when i made the function a class
+fiya = webdriver.Chrome("/Users/NewUser/Downloads/Automation/chromedriver")
+fiya.get("http://www.moat.com")
+searchElement = fiya.find_element_by_id("pro-landing-search-box")
+searchElement2 = fiya.find_element_by_xpath('//*[@id="search-bar"]/div/div[1]/span/a[1]').click()
 
+searchElement.send_keys("cocaine" + Keys.RETURN)
+print(searchElement2)
 
+"""
+//*[@id="search-bar"]/div/div[1]/span
 
-class shitWorks :
+//*[@id="search-bar"]/div/div[1]/span/a[1]
+//*[@id="search-bar"]/div/div[1]/span/a[3]
 
-    urlConnect = http.client.HTTPConnection("moat.com")
-    urlConnect.request("GET" , "/")
-    var = urlConnect.getresponse()
-    print (var.status)
-    print ('what th ')
-    #http: // newtours.demoaut.com / mercuryunderconst.php
-
-# think below was an attempt to strip the beginning of an url because method didn't like it
-# so it was a lil annoying
-
-"""urlConnection = http.client.HTTPConnection(mTryLink1.get_attribute('href').lstrip('https://'))
-urlConnection.request('GET',"")
-print(mTryLink1.get_attribute('href'))
-print(mTryLink1.get_attribute('href').lstrip('https://'))
-print(urlConnection.getresponse().status)
+//*[@id="popup-template"]/div/div[1]/div[2]/table/tbody/tr[5]/td[2]/div/a
 """
