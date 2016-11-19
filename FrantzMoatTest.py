5b9ec490077c44d4c4df6e5489a6e49e71ccfb71
Frantz <Frantz-b@github.com>
2016-11-19 01:54:13 -0500

    So first crack at grabbing elements for site_under_test

--- FrantzMoatTest.py
+++ FrantzMoatTest.py
@@ -1,13 +1,43 @@
-def comp(list1, list2):
-    for elementText in list1:
-        if elementText in list2:
-            return True
-        else:
-            return False
-            break
-list1 = ["moat.com/advertiser/ffw?report_type=display&creative_md5=9436b5e508dcafb24e7bbf05a8cf1d4b" , "moat.com/advertiser/ffw?report_type=display&creative_md5=e054cb32bd456de456f582577a8b9a85"]
-shit2 = [4 , 2]
-print(comp(list1 , shit2))
-def returnMatches(a, b):
-    return list(set(a) & set(b))
-print(returnMatches(list1, shit2))
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+chromeDriverLoc = "/Users/NewUser/Downloads/Automation/chromedriver"
+mURL = 'http://www.moat.com'
+
+
+cdriver = webdriver.Chrome(chromeDriverLoc)
+cdriver.get(mURL)
+
+# captured Web Elements
+mSearchBox = cdriver.find_element_by_id("pro-landing-search-box")
+mTryLink1 = cdriver.find_element_by_xpath('//*[@id="search-bar"]/div/div[1]/span/a[1]')
+mTryLink2 = cdriver.find_element_by_xpath('//*[@id="search-bar"]/div/div[1]/span/a[2]')
+mTryLink3 = cdriver.find_element_by_xpath('//*[@id="search-bar"]/div/div[1]/span/a[3]')
+
+# List of captured Try These links
+mList = [mTryLink1.text, mTryLink2.text, mTryLink3.text]
+mList2 = [mTryLink1.text, mTryLink2.text, mTryLink3.text]
+
+def somehit(caplist , othlist):
+    #caplist = mList
+    print(caplist, othlist)
+somehit(mList , mList2)
+print(mTryLink2 , " ", mTryLink1)
+
+def verifyRandom(tryTheseList):
+    cdriver.refresh()
+
+    mTryLinkRefresh1 = cdriver.find_element_by_xpath('//*[@id="search-bar"]/div/div[1]/span/a[1]')
+    mTryLinkRefresh2 = cdriver.find_element_by_xpath('//*[@id="search-bar"]/div/div[1]/span/a[2]')
+    mTryLinkRefresh3 = cdriver.find_element_by_xpath('//*[@id="search-bar"]/div/div[1]/span/a[2]')
+    mList2 = [mTryLinkRefresh1.text, mTryLinkRefresh2.text, mTryLinkRefresh3.text]
+    list(set(tryTheseList) & set(mList2))
+    if not list:
+        print('Random Function:  PASSED')
+    else:
+        print('Random Function:  FAILED')
+
+mList3 = [mTryLink4.text, mTryLink5.text]
+mList4 = [mTryLink4.text, mTryLink5.text]
+somehit(mList3 , mList4)
+print(mTryLink5 , " ", mTryLink4)
