77908498e0c89b3ea20c0c6fa16c0eaf292e27da
Frantz <Frantz-b@github.com>
2016-11-19 02:33:17 -0500

    First big rewrite of everything coming together.  Worked on methods one at a time and cleaning them up.  Also made the methods in a fashion where you pass arguments to them

--- FrantzMoatTest.py
+++ FrantzMoatTest.py
@@ -1,139 +1,141 @@
 from selenium import webdriver
 import time
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 
+
 chromeDriverLoc = "/Users/NewUser/Downloads/Automation/chromedriver"
 mURL = 'http://www.moat.com'
+mSearchTerm = 'betterment'
+
+#Location of elements on main page (moat.com)
+mSearchBox = "pro-landing-search-box"  #ID of search box
+tryTheseLink1 = '//*[@id="search-bar"]/div/div[1]/span/a[1]'
+tryTheseLink2 = '//*[@id="search-bar"]/div/div[1]/span/a[2]'
+tryTheseLink3 = '//*[@id="search-bar"]/div/div[1]/span/a[3]'
+recentAds1 = '//*[@id="search-bar"]/div/div[2]/ul/li[1]/h4'
+recentAds2 = '//*[@id="search-bar"]/div/div[2]/ul/li[2]/h4'
+recentAds3 = '//*[@id="search-bar"]/div/div[2]/ul/li[3]/h4'
+recentAds4 = '//*[@id="search-bar"]/div/div[2]/ul/li[4]/h4'
+
+#Location of elements on search results page
+allegedSiteCreativeCount = '//*[@id="comp-header"]/div[1]/div[2]/span'
+popupCreativeLoc = '//*[@id="popup-template"]/div/div[1]/div[2]/table/tbody/tr[5]/td[2]/div/a'
+popupCreativeLinkLoc = '//*[@id="popup-template"]/div/div[1]/div[2]/table/tbody/tr[5]/td[2]/div/div/input'
 
 cdriver = webdriver.Chrome(chromeDriverLoc)
 cdriver.get(mURL)
 
-cdriver.find_element_by_id("pro-landing-search-box").send_keys('better' + Keys.RETURN)
-time.sleep(2)
-loadMeBtn = cdriver.find_element_by_id("paginate-button")
-x = 0
+# capturing Web Elements
+def tryTheseTextListCapture():
+    'Intended to capture list of Try These link Texts from Moat main page'
+    mTryText1 = cdriver.find_element_by_xpath(tryTheseLink1).text
+    mTryText2 = cdriver.find_element_by_xpath(tryTheseLink2).text
+    mTryText3 = cdriver.find_element_by_xpath(tryTheseLink3).text
+    mListTexts = [mTryText1, mTryText2, mTryText3]
+    return mListTexts
 
-insideBox = cdriver.find_element_by_class_name('ad-cell')
-hover = ActionChains(cdriver).move_to_element(insideBox)
-hover.perform()
-time.sleep(2)
-damn = cdriver.find_element_by_xpath('//*[@id="popup-template"]/div/div[1]/div[2]/table/tbody/tr[5]/td[2]/div/a')
-damn.click()
-damn2 = cdriver.find_element_by_xpath('//*[@id="popup-template"]/div/div[1]/div[2]/table/tbody/tr[5]/td[2]/div/div/input')
-print(damn2.is_displayed())
-"""
-while loadMeBtn.is_displayed() :
-    loadMeBtn.click()
-    x = x + 1
-    print("This was clicked :", x, " times")
-    time.sleep(4)
-insideBox = cdriver.find_elements_by_class_name('ad-cell')
-#insideBox = cdriver.find_element_by_css_selector('#all > div.ads-container > div.ads').get_attribute('div')
-print(len(insideBox))
-# shit comet ff fuck rocket zata zz tops (get # createvies by span via ele by css"""
+def tryTheseLinkListCaputure():
+    'Intended to capture list of Try These Links from Moat main page'
+    mTryLink1 = cdriver.find_element_by_xpath(tryTheseLink1).get_attribute('href')
+    mTryLink2 = cdriver.find_element_by_xpath(tryTheseLink2).get_attribute('href')
+    mTryLink3 = cdriver.find_element_by_xpath(tryTheseLink3).get_attribute('href')
+    mListLnks = [mTryLink1, mTryLink2, mTryLink3]
+    return mListLnks
 
+def recentAdDisplayTimeCapture():
+    'Intended to capture list of Recently Seen Ad Times from Moat main page'
+    recentAdsEle1 = cdriver.find_element_by_xpath(recentAds1).text
+    recentAdsEle2 = cdriver.find_element_by_xpath(recentAds2).text
+    recentAdsEle3 = cdriver.find_element_by_xpath(recentAds3).text
+    recentAdsEle4 = cdriver.find_element_by_xpath(recentAds4).text
+    mRecentAdList = [recentAdsEle1, recentAdsEle2, recentAdsEle3, recentAdsEle4]
+    return mRecentAdList
 
-"""from urllib.request import Request, urlopen
-from urllib.error import HTTPError
+# List for captured Try These links & text.  Also list of Recent Ads from Main Moat page
 
-mList2 = ['https://wwww.moat.com', 'http://moat.com', 'https://moat.com/search_lucky?q=%7C%7C&doc_type=advertiser']
-def verifyWorkingLnks(lnksList):
-    cnt = 0
-    for x in lnksList:
-        req = Request(x)
+#mListLnk = [mTryLink1.get_attribute('href'), mTryLink2.get_attribute('href'), mTryLink3.get_attribute('href')]
+#mListRecentAd = [recentAdsEle1.text, recentAdsEle2.text, recentAdsEle3.text, recentAdsEle4.text]
+
+def verifyRandom():
+    "Verifies list of Try These links are different from New list of links"
+    mList1 = tryTheseTextListCapture()
+    cdriver.refresh()
+    #Grabing New set of 'Try These' links
+    mList2 = tryTheseTextListCapture()
+    if not list(set(mList1) & set(mList2)):
+        print('Random Function:  PASSED')
+    else:
+        print('Random Function:  FAILED')
+
+def verifyWorkingLnks():
+    "Verifies each Try These link are can load a successful page. (http response = 200)"
+    lnksList = tryTheseLinkListCaputure()
+    count = 0
+    for lnk in lnksList:
+        req = Request(lnk)
         try:
-            response = urlopen(req)
+            urlopen(req)
         except HTTPError as e:
-            print('Link Validation Function FAILED with following lnk: ', x, ' Status code given: ', e.code)
+            print('Link Validation Function FAILED with following lnk: ', lnk, ' Status code given: ', e.code)
         else:
-            cnt += 1
-            if cnt == 3 :
+            count += 1
+            if count == 3 :
                 print('Link Validation Function:  PASSED')
 
-eleText = '22 mins ago'
-x = eleText.split()
-print(x)
-print(int(x[0]))
-num = int(x[0])
-if (any('min' in dic for dic in x)) & (num <30) :
-    print('exitcue')
-else:
-    print('what thsfuk')
+def verifyRecentAdTime():
+    'Verifies list of Recently Seen Ads displayed are less than 30mins fresh'
+    recentAdverList = recentAdDisplayTimeCapture()
+    count = 0
+    for recentAd in recentAdverList :
+        rAdList = recentAd.split()
+        rNumber = float(rAdList[0])
+        if (any('min' in partial for partial in rAdList)) & (rNumber < 30) : count += 1
+        elif (any('sec' in partial for partial in rAdList)) & (rNumber < 60*30) : count += 1
+        elif (any('hour' in partial for partial in rAdList)) & (rNumber < 0.5): count += 1
+    if count == 4 : print("Recently Seen Ads Function:  PASSED")
+    else: print("Recently Seen Ads Function:  FAILED")
 
-mylist = ["2 min ago", "54 secs ago", '30 secs ago', '0.5 hours ago']
+def verifySearchResultsAdCount(searchTerm):
+    'Verifies number of Ads displayed matches the count listed at top of Search Results'
+    cdriver.find_element_by_id(mSearchBox).send_keys(searchTerm + Keys.RETURN)
+    time.sleep(2)
 
-def verifyRecentAdTime(AdverList):
-    episolon = 0
-    for x in AdverList:
-        p = x.split()
-        n = float(p[0])
-        if (any('min' in c for c in p)) & (n < 30) :
-            episolon += 1
-        elif (any('sec' in c for c in p)) & (n < 60*30) :
-            episolon += 1
-        elif (any('hour' in c for c in p)) & (n < 0.5):
-            episolon += 1
-    if episolon == 4 :
-        print("pass")
-    else: print('fail', episolon)
+    creativeCount = cdriver.find_element_by_xpath(allegedSiteCreativeCount).text
+    creativeCountList = creativeCount.split()
+    creativeCountNum = int(creativeCountList[0])
+    loadMeBtn = cdriver.find_element_by_id("paginate-button")
+    while loadMeBtn.is_displayed():
+        loadMeBtn.click()
+        time.sleep(4)
+    creativesListDisplayed = cdriver.find_elements_by_class_name('ad-cell')
 
-verifyRecentAdTime(mylist)
+    if len(creativesListDisplayed) == creativeCountNum : print('Search Results Ad Count Function:  PASSED')
+    else : print('Search Results Ad Count:  FAILED')
 
-//*[@id="popup-template"]/div
-//*[@id="popup-template"]/div/div[1]/div[1]/div/img
-//*[@id="popup-template"]/div/div[1]/div[2]
-//*[@id="popup-template"]/div/div[1]/div[2]"""
+def verifyShareAd(searchTerm):
+    'Verifies Share Ad feature works by being able to view the link of current creative'
+    cdriver.get(mURL)
+    cdriver.find_element_by_id(mSearchBox).send_keys(searchTerm + Keys.RETURN)
+    time.sleep(2)
 
-"""from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+    creativesListDisplayed = cdriver.find_element_by_class_name('ad-cell')
+    mousehover = ActionChains(cdriver).move_to_element(creativesListDisplayed)
+    mousehover.perform()
+    time.sleep(2)
+    popupCreative = cdriver.find_element_by_xpath(popupCreativeLoc)
+    popupCreative.click()
+    popupCreativeLink = cdriver.find_element_by_xpath(popupCreativeLinkLoc)
 
-chromeDriverLoc = "/Users/NewUser/Downloads/Automation/chromedriver"
-mURL = 'https://www.tumblr.com/search/naruto'
+    if popupCreativeLink.is_displayed() : print("Share This Ad Function:  PASSED")
+    else : print('Share This Ad Function:  FAILED')
 
-cdriver = webdriver.Chrome(chromeDriverLoc)
-cdriver.get(mURL)
+verifyRandom()
+verifyWorkingLnks()
+verifyRecentAdTime()
+verifySearchResultsAdCount(mSearchTerm)
+verifyShareAd(mSearchTerm)
 
-#cdriver.find_element_by_id("pro-landing-search-box").send_keys('Betterment' + Keys.RETURN)
-insideBox = cdriver.find_elements_by_tag_name('article')
-print(len(insideBox))"""
-
-#labels = driver.find_elements_by_tag_name("label")
-#inputs = driver.execute_script(
-#    "var labels = arguments[0], inputs = []; for (var i=0; i < labels.length; i++){" +
-#   "inputs.push(document.getElementById(labels[i].getAttribute('for'))); } return inputs;", labels)
-
-import unittest
-import selenium
-import page
-
-class PythonOrgSearch(unittest.TestCase):
-    """A sample test class to show how page object works"""
-
-    def setUp(self):
-        self.driver = selenium.webdriver.Firefox()
-        self.driver.get("http://www.python.org")
-
-    def test_search_in_python_org(self):
-        """
-        Tests python.org search feature. Searches for the word "pycon" then verified that some results show up.
-        Note that it does not look for any particular text in search results page. This test verifies that
-        the results were not empty.
-        """
-
-        #Load the main page. In this case the home page of Python.org.
-        main_page = page.MainPage(self.driver)
-        #Checks if the word "Python" is in title
-        assert main_page.is_title_matches(), "python.org title doesn't match."
-        #Sets the text of search textbox to "pycon"
-        main_page.search_text_element = "pycon"
-        main_page.click_go_button()
-        search_results_page = page.SearchResultsPage(self.driver)
-        #Verifies that the results page is not empty
-            assert search_results_page.is_results_found(), "No results found."
-
-    def tearDown(self):
-        self.driver.close()
-
-if __name__ == "__main__":
-    unittest.main()
+#cdriver.quit()
